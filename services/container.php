5041274822372ff1b21ace178c3c825b0f500b66
salva <salvador.palmiciano@gmail.com>
2016-01-23 14:35:58 -0300

    use env vars

--- services/container.php
+++ services/container.php
@@ -7,18 +7,18 @@ $container = new Pimple;
 
 $container['db'] = $container->share(function($container) {
 
-    $dsn = sprintf(
-        '%s:host=%s;port=%s;dbname=%s',
+    $db_host = (string)(getenv('DB_HOST')) ? getenv('DB_HOST') : DB_HOST;
+    $db_name = (string)(getenv('DB_NAME')) ? getenv('DB_NAME') : DB_NAME;
+    $db_user = (string)(getenv('DB_USER')) ? getenv('DB_USER') : DB_USER;
+    $db_pass = (string)(getenv('DB_PASS')) ? getenv('DB_PASS') : DB_PASS;
+
+    $dsn = sprintf('%s:host=%s;port=%s;dbname=%s',
         'mysql',
-        ENV['DB_HOST'],
+        $db_host,
         '3306',
-        ENV['DB_NAME']
+        $db_name
     );
-
-    $user = ENV['DB_USER'];
-    $pass = ENV['DB_PASS'];
-
-    RedBeanPHP\R::setup($dsn, $user, $pass);
+    RedBeanPHP\R::setup($dsn, $db_user, $db_pass);
     return new RedBeanPHP\R;
 });
 
