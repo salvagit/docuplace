3ae6f60ace6946cb0587d9f64d2a73b5e849bd86
salva <s@salvadorp.com>
2016-10-24 11:50:46 -0300

    improve form controller code

--- controllers/forms.js
+++ controllers/forms.js
@@ -4,101 +4,119 @@ const express = require('express');
 const fs = require('fs');
 const handlebars = require('handlebars');
 
+let db;
+
 module.exports = (db) => {
-
+	this.db = db;
 	let forms = express.Router();
-
-	// method:get
-	forms.get("/:form*?", (req, res) => {
-
-		if (req.params && req.params.form) {
-			((coll, db) => {
-				return new Promise((resolve, reject) => {
-					let collection = db.collection(coll);
-					collection.find({},{}, (err, docs) => {
-						if(err) console.log(err);
-						else {
-							(()=>{
-								return new Promise ((resolve,reject) => {
-									fs.readFile( __dirname + '/../views/row_menu.handlebars', 'utf8', (err, source) =>
-										(err) ? reject(err) : resolve(source)
-									);
-								});
-							})()
-							.then(
-								(source) => {
-									for (let key in docs) {
-										let template = handlebars.compile(source);
-										docs[key].actions = template({ id : docs[key]._id });
-									}
-									resolve(docs);
-								},
-								err => console.log(err)
-							);
-						}
-					});
-				});
-			})(req.params.form, db)
-			.then(
-				(data) => res.json(data),
-				(err) => res.json(err)
-			);
-
-		} else {
-			((db) => {
-				return new Promise((resolve, reject) => {
-					let collections = [];
-					db.getCollectionNames((err, colNames) => {
-						if (err) reject(err);
-						for (let i = 0; i < colNames.length; i++) {
-							collections.push({name: colNames[i]} );
-						}
-						resolve(collections);
-					});
-				});
-			})(db)
-			.then(
-				(data) => res.json(data),
-				(err) => res.json(err)
-			);
-		}
-	});
-
-	// method:post
-	forms.post("/:form*?", (req, res) => {
-		let form = req.params.form;
-		if (form) {
-			let data = {};
-			for (let k in req.body) data[req.body[k].name] = req.body[k].value;
-			((coll, data,db) => {
-				return new Promise((resolve, reject) => {
-					let collection = db.collection(coll);
-					collection.save(data,
-						(err, saved) => ( err || !saved ) ? reject(err) : resolve(saved)
-					);
-				});
-			})(form,data,db)
-			.then((data) => res.json(data));
-		} else {
-			((data, db) => {
-				return new Promise( (resolve, reject) => {
-					db.createCollection(data.name, (err, resp) => {
-						// console.log(data);
-						if (!err || 1 === resp.ok) {
-							let collection = db.collection(data.name),
-									schema = {};
-
-							data.fields.forEach( (el, index, arr) => schema[el.name] = '' );
-
-							collection.save(schema);
-							resolve(data);
-						} else reject(err);
-					});
-				});
-			})(req.body,db)
-			.then((data) => res.json(data));
-		}
-	});
-
+	// @todo find better way
+	forms.get("/:form*?", (req,res)=>get(req,res,db));
+	forms.post("/:form*?", (req,res)=>post(req,res,db));
 	return forms;
+};
+
+// method:get
+let get = (req,res,db) => {
+
+	let getForm = (coll, db) => {
+		return new Promise((resolve, reject) => {
+			let collection = db.collection(coll);
+			collection.find({},{}, (err, docs) => {
+				if(err) reject(err);
+				else {
+					(()=>{
+						/**
+						 * Get template.
+						 * @todo delegate 2 ui.
+						 */
+						return new Promise ( (resolve, reject) => {
+							fs.readFile( __dirname + '/../views/row_menu.handlebars', 'utf8',
+								(err, source) => (err) ? reject(err) : resolve(source)
+							);
+						});
+					})()
+					.then(
+						// prepare response.
+						(source) => {
+							for (let key in docs) {
+								let template = handlebars.compile(source);
+								docs[key].actions = template({ id : docs[key]._id });
+							}
+							resolve(docs);
+						},
+						err => console.log(err)
+					);
+				}
+			});
+		});
+	};
+
+	let getForms = (db) => {
+console.log(db);
+		return new Promise((resolve, reject) => {
+			let collections = [];
+			db.getCollectionNames((err, colNames) => {
+console.log(err, colNames);
+				if (err) reject(err);
+				for (let i = 0; i < colNames.length; i++) {
+					collections.push({name: colNames[i]} );
+				}
+				resolve(collections);
+			});
+		});
+	};
+	if (req.params && req.params.form) {
+		getForm(req.params.form, db)
+		.then(
+			(data) => res.json(data),
+			(err) => res.json(err)
+		);
+	} else {
+		getForms(db)
+		.then(
+			(data) => res.json(data),
+			(err) => res.json(err)
+		);
+	}
+};
+
+// method:post
+let post = (req,res) => {
+
+	let insertInForm = (coll,data,db) => {
+		return new Promise( (resolve, reject) => {
+			let collection = db.collection(coll);
+			collection.save(data,
+				(err, saved) => ( err || !saved ) ? reject(err) : resolve(saved)
+			);
+		});
+	};
+
+	let insertForm = (data, db) => {
+		return new Promise( (resolve, reject) => {
+			db.createCollection(data.name, (err, resp) => {
+				if (!err || 1 === resp.ok) {
+					let collection = db.collection(data.name),
+							schema = {};
+					// prepare schema.
+					data.fields.forEach( el => schema[el.name] = '' );
+					// save schema.
+					collection.save(schema);
+					// resolve promise.
+					resolve(data);
+				} else reject(err);
+			});
+		});
+	};
+
+	if (req.params && req.params.form) {
+		// get params.
+		let data = {};
+		for (let k in req.body) data[req.body[k].name] = req.body[k].value;
+		insertInForm(form,data,db)
+		.then((data) => res.json(data));
+	} else {
+		insertForm(req.body,db)
+		.then((data) => res.json(data));
+	}
 };
